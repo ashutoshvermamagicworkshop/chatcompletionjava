8fc1c143c030c25796933c49192ef18dd93528e9
ashutoshvermamagicworkshop <ashutosh.verma@magicworkshop.ai>
2025-07-11 17:02:18 +0530

    Create Config.java

---
+++ Config.java
@@ -0,0 +1,7 @@
+package chatCompletion;
+
+public interface Config {
+    String key = "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";
+    String url = "https://api.groq.com/openai/v1/chat/completions";
+    String model = "llama-3.3-70b-versatile";
+}
